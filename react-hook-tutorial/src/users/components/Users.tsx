608f01206bdea1e9a125d265a44a9d7e13733c20
jihanv <hedjmny@gmail.com>
2025-10-23 22:14:35 +0900

    Connect the input to a state value

--- react-hook-tutorial/src/users/components/Users.tsx
+++ react-hook-tutorial/src/users/components/Users.tsx
@@ -1,6 +1,9 @@
+import { useState } from "react"
 
 export default function Users() {
+
+    const [input, setInput] = useState("ererere")
     return (
-        <div><input /></div>
+        <div><input value={input} /></div>
     )
 }
