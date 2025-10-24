e8bef2ab35fe0760ad101e53b9646892082cbaf7
jihanv <hedjmny@gmail.com>
2025-10-24 20:13:24 +0900

    Add schema

--- react-hook-tutorial/src/users/components/Users.tsx
+++ react-hook-tutorial/src/users/components/Users.tsx
@@ -1,11 +1,17 @@
 import { useForm } from "react-hook-form"
 export default function Users() {
 
+
+    // Destructure the `errors` object from formState.
+    // `formState` contains the current status of the form (like validation errors, touched fields, isValid, etc.)
+    // Here we only extract `errors` to easily display validation messages for specific fields.
     const { register, formState: { errors }, handleSubmit } = useForm<{ email: string }>({ mode: "all" })
 
     const onSubmit = () => {
         console.log("submit")
     }
+
+    // handleSubmit(onSubmit) will only run if the form values are OK.
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
             <input {...register("email", {
@@ -14,7 +20,7 @@ export default function Users() {
                     message: "Email is required",
                 },
                 maxLength: {
-                    value: 10,
+                    value: 20,
                     message: "Too many characters",
                 }
             })}
