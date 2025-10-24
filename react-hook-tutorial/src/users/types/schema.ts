cb08d9792eb843cd614835ba899a22bf390506b0
jihanv <hedjmny@gmail.com>
2025-10-24 20:56:43 +0900

    Fix bug in email validation

--- react-hook-tutorial/src/users/types/schema.ts
+++ react-hook-tutorial/src/users/types/schema.ts
@@ -1,20 +1,15 @@
 import { z } from "zod";
 import { patterns } from "../../constants";
 
+// Validate the email string against the predefined regex in patterns.email.
+// Returns true if the input follows a valid email format, false otherwise.
 export const formSchema = z.object({
   name: z.string().min(1, { message: "Required" }),
   email: z
     .string()
     .min(1, { message: "Email is required" })
-    .refine((userInput) => {
-      // Validate the email string against the predefined regex in patterns.email.
-      // Returns true if the input follows a valid email format, false otherwise.
-      return (
-        patterns.email.test(userInput),
-        {
-          message: "Email not valid.",
-        }
-      );
+    .refine((userInput) => patterns.email.test(userInput), {
+      message: "Email not valid",
     }),
 });
 
